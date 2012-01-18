42f8a030e194f605e237f0689050c96ec508b752
orionempire <orionempire@gmail.com>
2012-01-17 19:07:39 -0500

    console and video done

---
+++ blue_fire_os/bluefire-00.00/bluefire-00.00.04/os/include/lib/string.h
@@ -0,0 +1,17 @@
+/**************************************************************************
+ *	bluefire-os
+ *	Version: 00.00.04
+ *	Author: David Davidson
+ *	Name: string.h
+ *	Created: Jan 12, 2012
+ *	Last Update: Jan 12, 2012
+ *	Purpose:
+ *  Usage:
+***************************************************************************/
+#ifndef STRING_H_
+#define STRING_H_
+
+// Public Function declarations
+u32int strlen(const s08int *str);
+u32int strnlen(const s08int *s, u32int count);
+#endif /* STRING_H_ */
