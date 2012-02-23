c2f00403201a3a9c842ca6c1a38d59b25004c4d4
orionempire <orionempire@gmail.com>
2012-02-23 14:16:01 -0500

    00.00.09 Checkpoint

---
+++ blue_fire_os/bluefire-00.00/bluefire-00.00.09/os/include/drivers/dma/dma.h
@@ -0,0 +1,19 @@
+/**************************************************************************
+ *	bluefire-os
+ *	Version: 00.00.00
+ *	Author: David Davidson
+ *	Name: dma.h
+ *	Created: Feb 14, 2012
+ *	Last Update: Feb 14, 2012
+ *	Purpose:
+ *  Usage:
+***************************************************************************/
+#ifndef DMA_H_
+#define DMA_H_
+
+#define DMA_MEMORY_DIMINSION (PHYSICAL_DMA_MEMORY_END-PHYSICAL_DMA_MEMORY_START)
+
+// Public Function declarations
+void initialize_DMA();
+
+#endif /* DMA_H_ */
